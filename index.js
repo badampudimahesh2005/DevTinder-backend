714dcad77d21a6eda3265aa0384ff28c54ba5719
Mahesh@Webdev <maheshbadampudi31@gmail.com>
2025-05-03 23:37:06 +0530

    signup->login->logout

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const connectDB = require("./config/database");
+const app = express();
+const cookieParser = require("cookie-parser");
+
+require("dotenv").config();
+
+
+
+app.use(express.json());
+app.use(cookieParser());
+
+const authRouter = require("./routes/auth");
+
+
+app.use("/", authRouter);
+
+
+
+connectDB()
+  .then(() => {
+    console.log("Database connection established...");
+    
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected!!");
+  });
+
+  app.listen(process.env.PORT, () => {
+    console.log("Server is successfully listening on port 5000...");
+  });
