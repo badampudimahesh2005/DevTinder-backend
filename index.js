e28f8b179cb7736992102617d799defe40e094f1
Mahesh@Webdev <maheshbadampudi31@gmail.com>
2025-05-04 11:59:44 +0530

    profile view and edit

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ app.use(express.json());
 app.use(cookieParser());
 
 const authRouter = require("./routes/auth");
-
+const profileRouter = require("./routes/profile");
 
 app.use("/", authRouter);
+app.use("/" , profileRouter);
 
 
 
