e2ed910cf22b941b2683247ecd26ddaf5fa103dd
Mahesh@Webdev <maheshbadampudi31@gmail.com>
2025-05-09 01:36:09 +0530

    fetching feed

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,7 @@ const { userAuth } = require('../middlewares/auth');
 const User = require('../models/user');
 
 
-const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
+const USER_SAFE_DATA = "_id firstName lastName profilePicture gender bio location age experienceLevel skills profileSummary education workExperience socialLinks";
 
 
 //Get all pending connections request fo the loggedInUser
@@ -63,4 +63,56 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     }   
 });
 
+
+userRouter.get("/feed", userAuth, async (req, res)=>{
+
+    //user should see all the cards except
+    //1. His own card
+    //2. His connections
+    //3. Ignored people
+    //4. Already sent the connection request
+
+    //if entry has done in connection request , they should not see each other
+    //like if one user send the req , other rejected it 
+
+    try{
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 30 ? 30 : limit;
+        const skip = (page - 1) * limit;
+
+        const loggedInUser= req.user;
+
+        const connectionRequests = await ConnectionRequest.find({
+            $or : [
+                {fromUserId: loggedInUser._id},
+                {toUserId: loggedInUser._id}
+            ]
+        }).select("toUserId fromUserId");
+
+        const hideUserFromFeed = new Set();
+
+        connectionRequests.map((connection)=>{
+            hideUserFromFeed.add(connection.fromUserId.toString());
+            hideUserFromFeed.add(connection.toUserId.toString());
+        })
+
+        hideUserFromFeed.add(loggedInUser._id.toString());  
+        const feedUsers = await User.find({
+            _id : {$nin : Array.from(hideUserFromFeed)}
+        })
+        .select(USER_SAFE_DATA)
+        .skip(skip)
+        .limit(limit);
+
+        res.json({
+            message: "Feed users fetched successfully",
+            data: feedUsers
+        })
+
+
+    }catch(err){
+        res.status(500).send("something went wrong: "+ err.message);
+    }
+})
 module.exports = userRouter;
